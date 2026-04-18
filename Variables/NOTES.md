03a373d3ac6d5e37d4b468e1291ff69dbc8af49b
jafarHussein <sanjaygreg19@gmail.com>
2026-04-18 22:01:05 +0300

    lesson 1 is done

---
+++ Variables/NOTES.md
@@ -0,0 +1,2 @@
+variable= A container for a value(String, integer, float, boolean).A variable behaves as if it was the value it contains
+
---
+++ Variables/main.py
@@ -0,0 +1,47 @@
+#String data types
+
+first_name="Jafar"
+food="pizza"
+email="123fake@gmail.com"
+
+
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"The email that you used for your job application is {email}")
+
+#Integers
+
+age=25
+print(f"You are {age} years old")
+
+quantity=13
+print(f"You are buying 13 plates for the party")
+
+
+students_number=35
+print(f"You class has {students_number} students")
+
+#Floats-Numbers that contain decimal point portions
+
+shoe_price=34.76
+print(f"The shoes go for ${shoe_price}")
+
+gpa=3.2
+print(f"Your gpa is {gpa}") 
+
+#Boolean-The boolean data has only two options, True or False, they are not outputed directly they are most used in conditional statements
+
+is_student=True
+
+if is_student:
+    print("You haven't graduated yet, you are still a student")
+else:
+    print("You are no longer a student")
+    
+for_sale=True
+
+if for_sale:
+    print("The item is for sale")
+else:
+    print("The item is not for sale")
+    
