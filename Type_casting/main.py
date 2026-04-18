ad006359367fb1111b43e69b30794b2f5fedc6b9
jafarHussein <sanjaygreg19@gmail.com>
2026-04-18 22:10:31 +0300

    lesson 2 Type casting

---
+++ Type_casting/main.py
@@ -0,0 +1,30 @@
+# Typecasting is achived using functions str(), int(), float(), bool()
+
+full_name="Jafar Hussein"
+age=25
+gpa=3.2
+is_student=True
+
+# Inorder to display the data type of a variable , you can use the type() function
+print(type(full_name))
+print(type(age))
+print(type(age))
+print(type(is_student))
+
+#Type casting an integer to a float
+float_age=float(age)
+print(float_age)
+print(type(float_age))
+
+#Type casting an integer to a string
+string_age=str(age)
+print(string_age)
+print(type(string_age))
+
+#Type casting a string to a Boolean
+boolean_name=bool(full_name)
+print(boolean_name)
+print(type(boolean_name))
+
+
+
