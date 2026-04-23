7c03cb6a69374987747553c4db3499872a1e6fa8
jafarHussein <sanjaygreg19@gmail.com>
2026-04-23 14:06:14 +0300

    in the past i really struggled with this concept but now we have it fully understood

---
+++ nested_loops/main.py
@@ -0,0 +1,8 @@
+# nested loop= A loop inside another loop(ouuter, inner)
+#       outter loop:
+#                inner loop:
+
+for counter_1 in range(1,3):
+    for counter_2 in range(1,11):
+        print(counter_2)
+    print()
