5e92dcc2b400dfa2d5920695c43bdbd7a9b48b86
jafarHussein <sanjaygreg19@gmail.com>
2026-05-01 22:23:37 +0300

    we just fixed the bugs we added yesterday, but the password scoring model really sucks

--- password_strength/main.py
+++ password_strength/main.py
@@ -1,113 +1,111 @@
-# Lets a make a simple programme that checks how strong your password is 
+# lets build a password strength checker
 import random
 import string
 
-#Variables
+#Variable declaration
 lower_chars='abcdefghijklmnopqrstuvwxyz'
 upper_chars='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-number_chars='1234567890'
+digit_chars='1234567890'
 symbol_chars='~!@#$%^&*()_+'
 has_lower=False
 has_upper=False
-has_symbols=False
 has_digits=False
+has_symbols=False
 password_score=0
 
-print("A py programme that checks how strong your password is")
-
 #Accepting user input
+print("----This is a simple py programme that checks how strong your password is ----")
 user_password=input("Please enter your password: ")
 
-while len(user_password) == 0:
-    print("Your password cannot be empty")
+if len(user_password)== 0 or user_password==' ':
+    print("No feedback provided ❗")
     user_password=input("Please enter your password: ")
-    
-while len(user_password)>0:  
-    if len(user_password)>0 and len(user_password)<6:
-        print(f"Your password length is {len(user_password)}, thats a very weak password")
+while len(user_password)>0:
+    if len(user_password)<6:
+        print(f'Your password length is {len(user_password)} characters long, thats a very weak password')
+        user_passwor=input("Please provide another password: ")
     elif len(user_password)>=6 and len(user_password)<=7:
-        print(f"Your password length is {len(user_password)}, thats a weak password")
-    elif len(user_password)>=8 and len(user_password)<=11:
-        print(f"Your password length is {len(user_password)}, thats a moderately strong password")
-        password_score+=1
-        break
-    elif len(user_password)>=12 and len(user_password)<=15:
-        print(f"Your password length is {len(user_password)}, thats a  strong password")
+        print(f"Your password length is {len(user_password)} characters long, thats a weak password")
+        user_password=input("Please provide another password")
+    elif len(user_password)>=8 and len(user_password)<=15:
+        print(f"Your password length is {len(user_password)} characters long, thats a strong password")
         password_score+=2
         break
-    elif len(user_password)>15:
-        print(f"Your password length is {len(user_password)}, thats a very strong password ")
+    elif len(user_password)>=16:
+        print(f"Your password length is {len(user_password)} characters long, thats a very strong password")
         password_score+=5
         break
+        
+#Check the password, lets avoid the generic stuff
+
+if user_password == 'password':
+    password_score-=1
     
-#Checking what characters are in the password string
+if user_password =='password123':
+    password_score-=1
+    
+if user_password == 'abcdef':
+    password_score-=1
+    
+if user_password == '12345':
+    password_score-=1
+    
 for letter in user_password:
     if letter in lower_chars:
-        has_lower=True
-    elif letter in upper_chars:
-        has_upper=True
-    elif letter in number_chars:
-        has_digits=True
-    elif letter in symbol_chars:
-        has_symbols=True
+        has_Lower=True
         
+for letter in user_password:
+    if letter in upper_chars:
+        has_upper=True
+        
+for letter in user_password:
+    if letter in digit_chars:
+        has_digits=True
+        
+for letter in user_password:
+    if letter in symbol_chars:
+        has_symbols=True
+                     
         
 if has_lower:
     password_score+=1
 
 if has_upper:
     password_score+=1
+      
+if has_digits:
+    password_score+=1
     
 if has_symbols:
     password_score+=1
     
-if has_digits:
-    password_score+=1
     
-if user_password == 'password':
-    password_score-=1
-elif user_password == 'aaaaaaa':
-    password_score-=1
-elif user_password == '12345':
-    password_score-=1
-elif user_password == 'abcdef':
-    password_score-=1
-elif user_password == 'qwerty':
-    password_score-=1
-    
-    
-if password_score <=0 and password_score<=2:
+if password_score <=2:
+    print(f"Your password: {user_password}")
     print(f"Your password score: {password_score}")
-    print("Your password is very weak")
-elif  password_score >=3 and password_score<=5:
+    print("Thats a very weak password")
+elif password_score >= 3 and password_score <= 5:
+    print(f"Your password: {user_password}")
     print(f"Your password score: {password_score}")
-    print("Your password is weak")
-elif password_score >=6 and password_score<=7:
-     print(f"Your password score: {password_score}")
-     print("Your password strength is medium")
-elif password_score > 7:
-     print(f"Your password score: {password_score}")
-     print("Your password is very strong")
-     
-     
-print(f"User password:{user_password}")
-print(f"Password score: {password_score}")
-
-user_feedback=''
-if password_score < 5:
-    user_feedback=input("Would you like me to generate a strong password(y/n): ").lower()
+    print("Thats moderately strong")
+elif password_score > 8:
+    print(f"Your password: {user_password}")
+    print(f"Your password score: {password_score}")
+    print("Thats a very strong password")
     
-if user_feedback == 'y':
-    password_length=int(input("How long would you like the password to be: "))
-    chars=list(string.punctuation + string.ascii_letters + string.digits + " ")
-    new_password=''
-    random.shuffle(chars)
-    for counter in range(0,password_length):
-        index=random.randint(0, len(chars))
-        new_password+=chars[index]
-    user_password=new_password
-    print("Your new strong password has been generated succefully ✅")
-    print(f"new password: {user_password}")
-else:
-    print("Byee, have a lovely day")
-        
+    
+if password_score > 0 and password_score<=3:
+    user_feedback=input("Would you like me to generate a new password(y/n): ").lower()
+    if user_feedback == 'y':
+        password_length=int(input("How long should the password be: "))
+        chars=list(string.punctuation + string.ascii_letters + string.digits)
+        new_password=''
+        key=chars.copy()
+        random.shuffle(key)
+        for counter in range(1, password_length):
+            new_password+=key[counter]
+        user_password=new_password
+        print("Password has been succefully generated")
+        print(f"new password: {user_password}")
+    else:
+        print("You have a lovely day")
