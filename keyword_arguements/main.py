6226abf80236a8e287d08d7be5a31f5fcc90097d
jafarHussein <sanjaygreg19@gmail.com>
2026-05-03 17:31:16 +0300

    positional arguements should come before key word arguements

--- keyword_arguements/main.py
+++ keyword_arguements/main.py
@@ -1,1 +1,10 @@
 # Keyword arguements in py
+# keyword arguements= an argument preceded by an identifier, helps with readability
+
+def hello(greeting, title, first_name, last_name):
+    print(f"{greeting}, {title} {first_name} {last_name}")
+    
+hello(greeting="Hello",title="Mr.",first_name="spongeBob",last_name="Squarepants")
+
+for counter in range(1,11):
+    print(counter, end="")
