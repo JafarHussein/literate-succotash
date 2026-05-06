4a445f47e65a8153107946eb6c8104c41c2c5260
jafarHussein <sanjaygreg19@gmail.com>
2026-05-06 20:21:04 +0300

    iterables in py

---
+++ iterables/main.py
@@ -0,0 +1,24 @@
+# Iterable = An object/collection that can return its elements one at a time, allowing it to be iterated over in a loop
+
+#list
+numbers=[1,2,3,4,5]
+
+for number in numbers:
+    print(number)
+
+#sets  
+fruits={'apple','orange','banana', 'coconut'}
+
+for fruit in fruits:
+    print(fruit)
+    
+#Dictionaries
+my_dictionary ={ "A":"1", "B":"2", "C":"3"}
+for key in my_dictionary:
+    print(key)
+
+for value in my_dictionary.values():
+    print(value)
+    
+for key, value in my_dictionary.items():
+    print(f"{key} = {value}")
