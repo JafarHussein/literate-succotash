e44b689e9021aa2263dc60dc12b7a962ca0d8844
jafarHussein <sanjaygreg19@gmail.com>
2026-05-10 01:32:00 +0300

    inheritance

---
+++ inheritance/main.py
@@ -0,0 +1,44 @@
+# Inheritance- Allows a class to inherit attributes and methods from another class, helps with code reuseability and extensibility
+
+class Animal:
+    def __init__(self, name):
+        self.name=name
+        self.is_alive=True
+        
+    def eat(self):
+        print(f"{self.name} is eating")
+        
+    def sleep(self):
+        print(f"{self.name} is sleeping")
+        
+class Dog(Animal):
+    def speak(self):
+        print("WOOF")
+
+class Cat(Animal):
+    def speak(self):
+        print("MEOW")
+        
+class Mouse(Animal):
+    print("SQUEK")
+
+dog=Dog("Scooby")
+cat=Cat("Garfield")
+mouse=Mouse("Jerry")
+
+
+print(dog.name)
+print(dog.is_alive)
+dog.eat()
+dog.sleep()
+
+print(cat.name)
+print(cat.is_alive)
+cat.eat()
+cat.sleep()
+
+print(mouse.name)
+print(mouse.is_alive)
+mouse.eat()
+mouse.sleep()
+
