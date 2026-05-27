6f80e710a8272ab7cbb54dc77df9d05d1e5fa151
jafarHussein <sanjaygreg19@gmail.com>
2026-05-27 16:07:36 +0300

    nested classes

--- nested_classes/main.py
+++ nested_classes/main.py
@@ -1,4 +1,32 @@
-#Nested Classes
+# Nested class
 
-class Employee:
+class Company:
+    class Employee:
+        
+        def __init__(self, name,position):
+            self.name=name
+            self.position=position
+        
+        def get_details(self):
+            return f"{self.name}, {self.position}"
     
+    def __init__(self, name):
+        self.name=name
+        self.employees=[]
+        
+    def add_employees(self, name,position):
+        new_employee=self.Employee(name, position)
+        self.employees.append(new_employee)
+        
+    def list_employees(self):
+        return [employee.get_details()for employee in self.employees]
+    
+    
+company=Company("Krusty Krab")
+company.add_employees("Eugene Krabs","Manager")
+company.add_employees("Squirdward", "Cashier")
+
+#Printing employees in the company
+
+for employee in company.list_employees():
+    print(employee)
