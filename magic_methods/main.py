efc5d9de87901943a632d66ccd57833dde2be5c3
jafarHussein <sanjaygreg19@gmail.com>
2026-06-03 21:21:58 +0300

    Magic methods in py

---
+++ magic_methods/main.py
@@ -0,0 +1,27 @@
+# Magic methods= Dunder methods, __init__, __str__, __eql__ they are automatically by many of python's inbuilt operations.They allow developers to customize the behavior of objects
+
+
+class Book:
+    def __init__(self, title, author, number_of_pages):
+        self.title=title
+        self.author=author
+        self.number_of_pages=number_of_pages
+        
+    
+    def __str__(self):
+        return f"'{self.title}' by {self.author}"
+    
+    def __eq__(self, other):
+        return self.title == other.title and self.author == other.author
+    
+    
+    
+book1=Book("The Hobbit","J.R.R. Tolkien", 310)
+book2=Book("Harry Potter and the Philosopher stone","J.K.Rowling",223)
+book3=Book("The lion, witch and the wardrobe","C.S.Lewis", 172)
+
+print(book1)
+print(book2)
+print(book3)
+print(book1==book2)
+
